d8d9d27c718350fc3fd0124833b76c4bf2f8ee9e
Soorya-try <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>
2023-11-17 12:32:24 +1100

    Initial Commit

---
+++ flexy trial 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flexy trial 1
+//
+//  Created by Sooryagayathri on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flexy trial 1/flexy_trial_1App.swift
@@ -0,0 +1,17 @@
+//
+//  flexy_trial_1App.swift
+//  flexy trial 1
+//
+//  Created by Sooryagayathri on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct flexy_trial_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
